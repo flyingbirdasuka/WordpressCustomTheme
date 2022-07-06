255729a9115a3ec78ed5f5df79371ffffec46608
Asuka <72805418+flyingbirdasuka@users.noreply.github.com>
2022-07-06 05:58:31 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,21 +1,17 @@
  <!-- ======= Footer ======= -->
   <footer id="footer">
-
     <div class="container">
       <div class="copyright">
-        &copy; Copyright 2021 <strong><span>Asuka Method</span></strong>
+        &copy; Copyright 2022 <strong><span>Asuka Method</span></strong>
       <div class="credits">
-   
         Asuka Method is a registered trademark in Benelux <br>
         Designed by <a href="https://dev.asukamethod.com/">Asuka</a>
       </div>
     </div>
   </footer><!-- End Footer -->
 
-  <div id="preloader"></div>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
  <?php wp_footer(); ?>
   
-</body>
-
+ </body>
 </html>
