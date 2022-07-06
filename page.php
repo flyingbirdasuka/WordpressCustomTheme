a0b24c05d0b8826b6611fe3daa924df6ad258482
Asuka <72805418+flyingbirdasuka@users.noreply.github.com>
2022-07-06 06:01:04 +0200

    Update page.php

--- page.php
+++ page.php
@@ -3,26 +3,20 @@
     <br>
     <section id="about" class="about">
           <div class="container" data-aos="fade-up">
-
             <div class="row">
-
-              <h2><?php //echo get_the_title(); ?></h2>
-             
                 <?php
-
-                  
-
                   if( have_posts() ){
                     while ( have_posts() ){
                  	    the_post();
                  	    get_template_part('template-parts/content', 'page');
+                      if ( is_active_sidebar( 'home_right_1' ) ) : ?>
+                        <div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
+                          <?php dynamic_sidebar( 'home_right_1' ); ?>
+                        </div><!-- #primary-sidebar -->
+                      <?php endif; 
                     }
                   }
-                
-            
                 ?> 
-
             </div>
     </section><!-- End Section -->
-
 <?php get_footer(); ?>
